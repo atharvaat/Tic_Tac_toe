b59f1d722d9624c66efa791889ff08bc30fdaa9b
atharva <atharvatodankar.at@gmail.com>
2023-01-26 12:37:29 +0530

    Add: Adding program to allow the player to choose letter X or O

--- src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
+++ src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -1,16 +1,40 @@
 package com.bridgelabz.tictactoeprogram;
+import java.util.Scanner;
 
 public class TicTacToe {
-	 private static char[] board = new char[10];
+	 int i;
+		static char[] board = new char[10];  
+		static char player, computer;
+		Scanner scanner = new Scanner(System.in);
+
 		
-		static void startGame() {
-			for (int i = 1; i <= 9; i++) {
-				board[i] = 1;
-				System.out.println(board);
+		 
+		public void initialize() {
+			for (i = 1; i < 10; i++) {   
+				board[i] = ' ';
 			}
 		}
+		
+		public void check() { 
+			System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
+			char choice = scanner.next().charAt(0);
+			if (choice == 'X') {
+				player = 'X';
+				computer = 'O';
+			} else if (choice == 'Y') {
+				player = 'O';
+				computer = 'X';
+			}
+		}
+		
+		 
 		public static void main(String[] args) {
-			System.out.println("Welcome to Tic Tac Toe ");
-			startGame();
+			System.out.println("Welcome to Tic Tac Toe");
+			System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+			System.out.println("---------");
+			System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+			System.out.println("---------");
+			System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+			
 		}
 }
