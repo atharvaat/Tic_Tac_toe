8ffe5eeee9548e3b1dbf67bce7b6863a548c9218
atharva <atharvatodankar.at@gmail.com>
2023-01-26 12:30:20 +0530

    Adding program for Tic Tac Toe board

---
+++ src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.tictactoeprogram;
+
+public class TicTacToe {
+	 private static char[] board = new char[10];
+		
+		static void startGame() {
+			for (int i = 1; i <= 9; i++) {
+				board[i] = 1;
+				System.out.println(board);
+			}
+		}
+		public static void main(String[] args) {
+			System.out.println("Welcome to Tic Tac Toe ");
+			startGame();
+		}
+}
