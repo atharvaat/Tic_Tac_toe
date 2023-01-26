d87d65c1f10c1e28b7eae42d8fdd57b79e2faeb6
atharva <atharvatodankar.at@gmail.com>
2023-01-27 00:28:18 +0530

    Add: Adding program for ability to move to desired location in the board

--- src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
+++ src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -2,39 +2,70 @@ package com.bridgelabz.tictactoeprogram;
 import java.util.Scanner;
 
 public class TicTacToe {
-	 int i;
+	  int i;
 		static char[] board = new char[10];  
 		static char player, computer;
-		Scanner scanner = new Scanner(System.in);
+		private static int playLocation;
+		public static Scanner scannerObject = new Scanner(System.in); 
 
+		 public static void createBoard()
+		    {
+		        for (int index = 1; index < 10; index++)      
+		        {
+		            board[index] = ' ';
+		        }
+		    }
 		
-		 
-		public void initialize() {
-			for (i = 1; i < 10; i++) {   
-				board[i] = ' ';
-			}
-		}
-		
-		public void check() { 
-			System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
-			char choice = scanner.next().charAt(0);
-			if (choice == 'X') {
-				player = 'X';
-				computer = 'O';
-			} else if (choice == 'Y') {
-				player = 'O';
-				computer = 'X';
-			}
-		}
-		
-		 
-		public static void main(String[] args) {
-			System.out.println("Welcome to Tic Tac Toe");
-			System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-			System.out.println("---------");
-			System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-			System.out.println("---------");
-			System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-			
-		}
+		    public static void getPlayerChoice()
+		    {
+
+		        System.out.print("select X or O : ");
+		        player = scannerObject.next().toUpperCase().charAt(0);
+
+		        if (player == 'X')
+		            computer = 'O';
+		        else
+		            computer = 'X';
+		        System.out.println("You have selected : " + player);
+		        System.out.println("Computer's choice is : " + computer);
+		    }
+		   
+		    public static void showBoard()
+		    {
+		        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		        System.out.println("---------");
+		        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		        System.out.println("---------");
+		        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+		    }
+		    
+		    
+		    //  user is going to make a move to the desired location
+		    //  if it is not in range from 1 to 9
+		    //  then it will show you an invalid input
+		     
+		     public static void userMove()
+		    {
+		        System.out.println("Enter Location 1-9 to Make Move");
+		        playLocation = scannerObject.nextInt();
+		        if (playLocation < 10 && playLocation > 0)
+		        {
+		            board[playLocation] = player;
+		            showBoard();
+		        } else
+		        {
+		            System.out.println("Invalid Choice");
+		        }
+		    }
+
+		    public static void main(String[] args)
+		    {
+
+		        System.out.println("Welcome To Tic Tac Toe");
+		        createBoard();
+		        getPlayerChoice();
+		        showBoard();
+		        userMove();
+
+		    }
 }
