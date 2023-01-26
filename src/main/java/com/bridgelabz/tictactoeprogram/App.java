7046b99e29a36a61e3399d7311dc80d8cd8010ce
atharva <atharvatodankar.at@gmail.com>
2023-01-26 12:26:36 +0530

    Add: Wecome to Tic Tac Toe Program

---
+++ src/main/java/com/bridgelabz/tictactoeprogram/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.tictactoeprogram;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Tic Tac Toe Program" );
+    }
+}
