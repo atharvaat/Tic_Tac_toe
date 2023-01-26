75d522b85eff61414e7717f93744cb993f67ae37
atharva <atharvatodankar.at@gmail.com>
2023-01-27 00:37:32 +0530

    Add: Adding program to check free space before making desired move

--- src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
+++ src/main/java/com/bridgelabz/tictactoeprogram/TicTacToe.java
@@ -2,61 +2,75 @@ package com.bridgelabz.tictactoeprogram;
 import java.util.Scanner;
 
 public class TicTacToe {
-	  int i;
-		static char[] board = new char[10];  
+	 static char[] board = new char[10];  
 		static char player, computer;
 		private static int playLocation;
 		public static Scanner scannerObject = new Scanner(System.in); 
-
-		 public static void createBoard()
-		    {
+		
+		    /*
+		     *creating the game board
+		     *Select the index from 1 to 9 to make the move.
+		     */
+			 public static void createBoard()
+			 {
 		        for (int index = 1; index < 10; index++)      
 		        {
 		            board[index] = ' ';
 		        }
 		    }
-		
-		    public static void getPlayerChoice()
-		    {
+		 
+		    /*
+		     * This game has two player x and o
+		     */
+			 public static void getPlayerChoice()
+			 {
 
-		        System.out.print("select X or O : ");
-		        player = scannerObject.next().toUpperCase().charAt(0);
+				 System.out.print("select X or O : ");
+				 player = Character.toUpperCase(scannerObject.next().charAt(0));
 
-		        if (player == 'X')
+				 	if (player == 'X')
 		            computer = 'O';
-		        else
+				 	else
 		            computer = 'X';
-		        System.out.println("You have selected : " + player);
-		        System.out.println("Computer's choice is : " + computer);
-		    }
-		   
-		    public static void showBoard()
-		    {
-		        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-		        System.out.println("---------");
-		        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-		        System.out.println("---------");
-		        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-		    }
+				 	System.out.println("You have selected : " + player);
+				 	System.out.println("Computer's choice is : " + computer);
+			 }	
 		    
+			 	 public static void showBoard()
+			 	 {
+			 		 System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+			 		 System.out.println("---------");
+			 		 System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+			 		 System.out.println("---------");
+			 		 System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+			 	 }
 		    
-		    //  user is going to make a move to the desired location
-		    //  if it is not in range from 1 to 9
-		    //  then it will show you an invalid input
+			 	 
+			 	 // user is going to make a move to the desired location
+			 	 // if it is not in range from 1 to 9
+			 	 // then it will show you an invalid input
+			 	  
+			 	 public static void userMove()
+			 	 {
+			 		 System.out.println("Enter Location 1-9 to Make Move");
+			 		 playLocation = scannerObject.nextInt();
+			 		 if (playLocation < 10 && playLocation > 0)
+			 		 {
+			 			 board[playLocation] = player;
+			 			 showBoard();
+			 		 } else
+			 		 {
+			 			 System.out.println("Invalid Choice");
+			 		 }
+		    }
 		     
-		     public static void userMove()
-		    {
-		        System.out.println("Enter Location 1-9 to Make Move");
-		        playLocation = scannerObject.nextInt();
-		        if (playLocation < 10 && playLocation > 0)
-		        {
-		            board[playLocation] = player;
-		            showBoard();
-		        } else
-		        {
-		            System.out.println("Invalid Choice");
-		        }
-		    }
+		    public static boolean isEmpty() {
+			       if (board[playLocation] == ' ') {
+			            return true;      
+			       } else {
+			            return false;
+			       }
+		    }   
 
 		    public static void main(String[] args)
 		    {
@@ -66,5 +80,7 @@ public class TicTacToe {
 		        getPlayerChoice();
 		        showBoard();
 		        userMove();
-		    }
+		        isEmpty();
+
+	        }
 }
